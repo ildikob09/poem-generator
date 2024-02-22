b595c8004765ee99312bde5bef0506b8f70ace10
ildikob09 <155365916+ildikob09@users.noreply.github.com>
2024-02-22 20:57:41 +0000

    added basic form handling and typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "Az én szivem sokat csatangolt,de most már okul és tanul.",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
