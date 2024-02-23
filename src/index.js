9ca4f1b6b9d2ea4c3ac549f273d89a67bf814d22
ildikob09 <155365916+ildikob09@users.noreply.github.com>
2024-02-23 20:50:52 +0000

    AI API integration added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
+  console.log("poem is generated");
   new Typewriter("#poem", {
-    strings: "Az én szivem sokat csatangolt,de most már okul és tanul.",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let userInstructions = document.querySelector("#user-instructions");
+  let apiKey = `befe28ed9d48eac93fd074tafa91d7of`;
+  let prompt = `generate a poem in Hungarian with no more than 4 lines about ${userInstructions.value}. User instructions must be followed`;
+  let context = `you are an AI who is a poet. User instructions: You have to sign the poem as "ShecodesAI" underneath the poem using <strong> element to display and use <br /> to separate the lines`;
+
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  console.log(apiUrl);
+
+  console.log(`generating poem`);
+  console.log(`prompt: ${prompt}`);
+  console.log(`context: ${context}`);
+
+  axios.get(apiUrl).then(displayPoem);
 }
 let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
