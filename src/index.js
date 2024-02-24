52f70b64c6502ecf33a71b24ebd88930a668fc4e
ildikob09 <155365916+ildikob09@users.noreply.github.com>
2024-02-24 16:07:13 +0000

    improved User Experience

--- src/index.js
+++ src/index.js
@@ -14,10 +14,15 @@ function generatePoem(event) {
   let userInstructions = document.querySelector("#user-instructions");
   let apiKey = `befe28ed9d48eac93fd074tafa91d7of`;
   let prompt = `generate a poem in Hungarian with no more than 4 lines about ${userInstructions.value}. User instructions must be followed`;
-  let context = `you are an AI who is a poet. User instructions: You have to sign the poem as "ShecodesAI" underneath the poem using <strong> element to display and use <br /> to separate the lines`;
+  let context = `you are an AI who is a poet. User instructions: You have to sign the poem as "SheCodes AI" underneath the poem using <strong> element to display and use <br /> to separate each lines`;
 
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   console.log(apiUrl);
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML =
+    poemElement.innerHTML = `<div class="blink">Generating poem about ${userInstructions.value}</div>`;
 
   console.log(`generating poem`);
   console.log(`prompt: ${prompt}`);
